85cd468682f72286fc5d492f15af12903f9fa89a
charmainenotlowloh <e0203409@u.nus.edu>
2020-04-12 16:53:07 +0800

    grpc

--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module backend
 go 1.14
 
 require (
-	github.com/go-chi/chi v4.1.0+incompatible // indirect
-	github.com/lib/pq v1.3.0 // indirect
+	github.com/go-chi/chi v4.1.0+incompatible
+	github.com/golang/protobuf v1.3.3
+	github.com/lib/pq v1.3.0
+	golang.org/x/net v0.0.0-20190311183353-d8887717615a
+	google.golang.org/grpc v1.28.1
 )
--- server.go
+++ server.go
@@ -4,11 +4,23 @@ import (
 	"database/sql"
 	"fmt"
 	"net/http"
+	"net"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 	_ "github.com/lib/pq"
+	"context"
+	"flock-support/back/proto"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	
+
 )
+
+type server struct{}
+
+
 
 var router *chi.Mux
 var db *sql.DB
@@ -59,8 +71,41 @@ func AllPosts(w http.ResponseWriter, r *http.Request) {
 	err = result.Err()
 }
 
+// func main() {
+// 	defer db.Close()
+// 	routers()
+// 	http.ListenAndServe(":8005", router)	
+// }
 func main() {
-	defer db.Close()
-	routers()
-	http.ListenAndServe(":8005", router)
+	//listener, err := net.Listen("tcp", ":4040")
+	listener, err := net.Listen("tcp", ":8005")
+	if err != nil {
+		panic(err)
+	}
+
+	srv := grpc.NewServer()
+	proto.RegisterAddServiceServer(srv, &server{})
+	reflection.Register(srv)
+
+	if e := srv.Serve(listener); e != nil {
+		panic(e)
+	}
+
 }
+
+func (s *server) Add(ctx context.Context, request *proto.Request) (*proto.Response, error) {
+	a, b := request.GetA(), request.GetB()
+
+	result := a + b
+
+	return &proto.Response{Result: result}, nil
+}
+
+func (s *server) Multiply(ctx context.Context, request *proto.Request) (*proto.Response, error) {
+	a, b := request.GetA(), request.GetB()
+
+	result := a * b
+
+	return &proto.Response{Result: result}, nil
+}
+
